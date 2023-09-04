97c5ff742614f718504de52b44d6d68c46763d0f
AP-Code <apcode@proton.me>
2023-09-04 10:31:24 +0200

    Added Task lists

---
+++ frontend/components/TaskCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLink :to="`/tasks/${task.id}`">
+    <div class="bg-white p-4 rounded-lg shadow-md mb-4">
+      <h3 class="text-xl font-bold mb-2">{{ task.title }}</h3>
+      <div class="text-sm text-gray-600 mb-2">
+        <strong>Deadline:</strong> {{ task.deadlineDate }} at {{ task.deadlineTime }}
+      </div>
+      <p class="text-gray-700 truncate-2-lines">{{ task.description }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
